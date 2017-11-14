4da547851e1de28e247afb3f69b0e87edac33051
Shayane Alcântara <33501171+shayanealcantara@users.noreply.github.com>
2017-11-14 10:53:51 -0200

    Update Turma.java

--- src/dados/Turma.java
+++ src/dados/Turma.java
@@ -2,4 +2,57 @@ package dados;
 
 public class Turma {
 
+	private String disciplina;
+	private Aluno alunos;
+	private int quant_Alunos;
+	
+	public String getDisciplina() {
+		return disciplina;
+	}
+  
+	public void setDisciplina(String disciplina) {
+		if(disciplina.equals(" ")){
+			try{
+				throw new DisciplinaEmBrancoException("Insira uma disciplina");
+			} catch DisciplinaEmBrancoException(disciplina d)
+		}
+		this.disciplina = disciplina; 
+	}
+  
+	public Aluno getAlunos() {
+		return alunos;
+	}
+  
+	public void setAlunos(Aluno alunos) {
+		this.alunos = alunos;
+	}
+	
+	public void matricular(Aluno a){
+		
+		if(this.quant_Alunos < 45){
+			a.add();
+		}
+   
+		try {
+			if(this.quant_Alunos > 45){
+				throw new TurmaCheiaException();
+			}
+      
+		} catch (TurmaCheiaException e) {
+			System.out.println("Turma cheia.");
+		}
+	}
+	
+	public void desmatricular(Aluno a){
+		
+		try{
+		if(this.quant_Alunos == 0){
+			throw new TurmaVaziaException();
+		} catch (TurmaVaziaException e){
+			System.out.println("Turma vazia, não há alunos para desmatricular.");
+		}
+	
+		}
+	}
+}
 }
